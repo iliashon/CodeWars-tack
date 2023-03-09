33701ed0578e54d207cd06adba282dccab4d71be
Ilya Tupalski <itupalski@gmail.com>
2023-03-09 12:52:46 +0400

    feat: add function 'Remove String Spaces'

---
+++ 8kyu/Remove-String-Spaces.js
@@ -0,0 +1,9 @@
+function noSpace(x){
+    return (x.replace(/\s/g,''));
+    }
+
+    console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+   
+ // Simple, remove the spaces from the string, then return the resultant string.
+
+//  https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
